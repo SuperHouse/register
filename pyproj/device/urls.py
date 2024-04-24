988355cb11bd728c21ea75279430579a58b4fcd1
Mitch Davis <mjd@afork.com>
2024-04-24 16:32:54 +1000

    Added a device action

--- pyproj/device/urls.py
+++ pyproj/device/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.top, name='top'),
     path('action/', views.general_action, name='general_action'),
     path("<int:device_number>/", views.device_detail, name='device_detail'),
+    path("<int:device_number>/action/", views.device_action, name='device_action'),
 ]
