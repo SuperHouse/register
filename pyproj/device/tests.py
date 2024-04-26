ea26170f8219b28fc9b3fa7e418658ec9bcf63ee
Mitch Davis <mjd@afork.com>
2024-04-26 11:39:40 +1000

    Added some simple tests which use pytest

--- pyproj/device/tests.py
+++ pyproj/device/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from django.urls import reverse
+
+
+@pytest.mark.django_db
+def test_view_top(client):
+    response = client.get(reverse('device:top'))
+    assert response.status_code == 200
+
+    assert '<h1>SuperHouse Device Register</h1>' in response.content.decode('utf-8')
+
+
+# Does the admin interface cause a redirect for a normal user?
+@pytest.mark.django_db
+def test_unauthorized(client):
+    url = reverse('admin:index')
+    response = client.get(url)
+    assert response.status_code == 302
+
+
+# Does the admin interface work ok for a superuser?
+@pytest.mark.django_db
+def test_superuser_view(admin_client):
+    url = reverse('admin:index')
+    response = admin_client.get(url)
+    assert response.status_code == 200
