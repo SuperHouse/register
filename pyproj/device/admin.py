aec5ab651317bce44a4a4e5eecc3977709cbd760
Mitch Davis <mjd@afork.com>
2024-05-14 10:01:54 +1000

    Added TestRecord model for device test records

--- pyproj/device/admin.py
+++ pyproj/device/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from .models import Client, Design, Device
+from .models import Client, Design, Device, TestRecord
 
 admin.site.register(Client)
 admin.site.register(Design)
 admin.site.register(Device)
+admin.site.register(TestRecord)
 
 admin.site.site_header = 'Device register'
 admin.site.site_title = 'SuperHouse device register'
--- pyproj/device/models.py
+++ pyproj/device/models.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, datetime
 
 from django.conf import settings
 from django.db import models
@@ -33,3 +33,37 @@ class Device(models.Model):
 
     def __str__(self):
         return f'{self.design.sku} (sn: {self.pk})'
+
+
+class TestRecord(models.Model):
+    NEW = 'NEW'
+    PASS = 'PASS'
+    FAIL = 'FAIL'
+    INCONCLUSIVE = 'HUH?'
+
+    RESULT_CHOICES = [
+        (NEW, 'STARTED'),
+        (PASS, 'PASSED'),
+        (FAIL, 'FAILED'),
+        (INCONCLUSIVE, 'INCONCLUSIVE'),
+    ]
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
+    test_dt = models.DateTimeField(default=datetime.now)
+    result = models.CharField(max_length=5, choices=RESULT_CHOICES, default='NEW')
+    notes = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.device} - {self.test_dt} - {self.result}'
+
+    def get_bootstrap_table_class(self):
+        classes = {
+            self.NEW: 'table-light',
+            self.PASS: 'table-success',
+            self.FAIL: 'table-danger',
+            self.INCONCLUSIVE: 'table-warning',
+        }
+
+        return classes.get(self.result, 'table-dark')
+
+
+
