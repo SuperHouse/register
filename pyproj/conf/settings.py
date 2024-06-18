750b0abffc30e797cfd64a10677992c1d107aacd
Mitch Davis <mjd@afork.com>
2024-06-18 11:00:48 +1000

    Add mail sending function

--- pyproj/conf/settings.py
+++ pyproj/conf/settings.py
@@ -225,6 +225,13 @@ BACKGROUND_SETTINGS = {
     },
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_TLS = True
+EMAIL_SSL = False
 
 try:
     from .local_settings import *
